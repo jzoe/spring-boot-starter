9b212700dc377bfe44c651c6cee9e6b8600753b3
15221499301@163.com <15221499301@163.com>
2017-11-21 00:20:56 +0800

    .

--- spring-boot-starter-quartz/src/main/java/com/quartz/model/TaskExecuter.java
+++ spring-boot-starter-quartz/src/main/java/com/quartz/model/TaskExecuter.java
@@ -1,7 +1,12 @@
 package com.quartz.model;
 
 import com.quartz.model.entity.QrtzTimedTask;
+import com.quartz.model.entity.QrtzTimedTaskParam;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class TaskExecuter {
 
@@ -10,12 +15,40 @@ public class TaskExecuter {
 
     private JdbcTemplate jdbcTemplate;
 
-    private String INSERT_SQL = "INSERT INTO " + taskTableName + "(TASK_ID, TASK_NAME, TASK_DESC, TASK_EXPRES, TASK_METHOD, TASK_CLASS, TASK_GROUP, STATUS, CREATE_TIME, CREATER)\n" +
-            "VALUES(3, 'QuartzTest03', 'quartz测试3', '0/5 * * * * ?', 'test03', 'com.github.quartz.examples.quartz.QuartzTest', 'quartzTest', 'U', sysdate, 'haha')";
+    private String INSERT_TASK_SQL = "INSERT INTO " + taskTableName + "(TASK_ID, TASK_NAME, TASK_DESC, TASK_EXPRES, TASK_METHOD, TASK_CLASS, TASK_GROUP, STATUS, CREATE_TIME, CREATER) " +
+            "VALUES(SEQ_TASK.NEXTVAL, ?, ?, ?, ?, ?, ?, ?, sysdate, ?)";
+    private String INSERT_TASK_PARAM_SQL = "INSERT INTO " + taskParamTableName + "(PARAM_ID, PARAM_KEY, PARAM_VALUE, PARAM_DESC, SORT_ID, TASK_NAME) " +
+            "VALUES (SEQ_TASK_PARAM.NEXTVAL, ?, ?, ?, ?, ?)";
+    private String UPDATE_TASK_SQL = "UPDATE " + taskTableName + " T SET {} WHERE T.TASK_NAME = ?";
+    private String UPDATE_TASK_PARAM_SQL = "UPDATE " + taskParamTableName + " T SET {} WHERE T.TASK_NAME = ? AND T.PARAM_KEY = ?";
 
     public void addTask(QrtzTimedTask qrtzTimedTask) {
-        String sql = "INSERT INTO ";
-        jdbcTemplate.execute("");
+        jdbcTemplate.update(INSERT_TASK_SQL, new PreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps) throws SQLException {
+                ps.setString(1, qrtzTimedTask.getTaskName());
+                ps.setString(2, qrtzTimedTask.getTaskDesc());
+                ps.setString(3, qrtzTimedTask.getTaskExpres());
+                ps.setString(4, qrtzTimedTask.getTaskMethod());
+                ps.setString(5, qrtzTimedTask.getTaskClass());
+                ps.setString(6, qrtzTimedTask.getTaskGroup());
+                ps.setString(7, "U");
+                ps.setString(8, qrtzTimedTask.getCreater());
+            }
+        });
+    }
+
+    public void addTaskParam(QrtzTimedTaskParam qrtzTimedTaskParam) {
+        jdbcTemplate.update(INSERT_TASK_PARAM_SQL, new PreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps) throws SQLException {
+                ps.setString(1, qrtzTimedTaskParam.getParamKey());
+                ps.setString(2, qrtzTimedTaskParam.getParamValue());
+                ps.setString(3, qrtzTimedTaskParam.getParamDesc());
+                ps.setLong(4, qrtzTimedTaskParam.getSortId());
+                ps.setString(5, qrtzTimedTaskParam.getTaskName());
+            }
+        });
     }
 
     public TaskExecuter setTaskTableName(String taskTableName) {
--- spring-boot-starter-quartz/src/main/resources/task_oracle.sql
+++ spring-boot-starter-quartz/src/main/resources/task_oracle.sql
@@ -1,116 +1,145 @@
--- Create table
-create table QRTZ_TIMED_TASK
+-- CREATE TABLE
+CREATE TABLE QRTZ_TIMED_TASK
 (
-  task_id     INTEGER not null,
-  task_name   VARCHAR2(200),
-  task_desc   VARCHAR2(500),
-  task_expres VARCHAR2(100),
-  task_method VARCHAR2(200),
-  task_class  VARCHAR2(200),
-  task_group  VARCHAR2(200) default 0,
-  status      VARCHAR2(100) default 'U',
-  create_time DATE default sysdate,
-  creater     VARCHAR2(200),
-  ext1        VARCHAR2(500),
-  ext2        VARCHAR2(500),
-  ext3        TIMESTAMP(6),
-  ext4        TIMESTAMP(6)
+  TASK_ID     INTEGER NOT NULL,
+  TASK_NAME   VARCHAR2(200),
+  TASK_DESC   VARCHAR2(500),
+  TASK_EXPRES VARCHAR2(100),
+  TASK_METHOD VARCHAR2(200),
+  TASK_CLASS  VARCHAR2(200),
+  TASK_GROUP  VARCHAR2(200) DEFAULT 0,
+  STATUS      VARCHAR2(100) DEFAULT 'U',
+  CREATE_TIME DATE DEFAULT SYSDATE,
+  CREATER     VARCHAR2(200),
+  EXT1        VARCHAR2(500),
+  EXT2        VARCHAR2(500),
+  EXT3        TIMESTAMP(6),
+  EXT4        TIMESTAMP(6)
 );
--- Add comments to the columns
-comment on column QRTZ_TIMED_TASK.task_id
-is '任务编码';
-comment on column QRTZ_TIMED_TASK.task_name
-is '任务名称';
-comment on column QRTZ_TIMED_TASK.task_desc
-is '任务描述';
-comment on column QRTZ_TIMED_TASK.task_expres
-is '任务执行表达式';
-comment on column QRTZ_TIMED_TASK.task_method
-is '任务执行方法';
-comment on column QRTZ_TIMED_TASK.task_class
-is '任务接口路径';
-comment on column QRTZ_TIMED_TASK.task_group
-is '任务分组';
-comment on column QRTZ_TIMED_TASK.status
-is '任务状态';
-comment on column QRTZ_TIMED_TASK.create_time
-is '创建时间';
-comment on column QRTZ_TIMED_TASK.creater
-is '创建人员';
-comment on column QRTZ_TIMED_TASK.ext1
-is '扩展字段1';
-comment on column QRTZ_TIMED_TASK.ext2
-is '扩展字段2';
-comment on column QRTZ_TIMED_TASK.ext3
-is '扩展字段3';
-comment on column QRTZ_TIMED_TASK.ext4
-is '扩展字段4';
--- Create/Recreate primary, unique and foreign key constraints
-alter table QRTZ_TIMED_TASK
-  add primary key (TASK_ID)
-  using index
-  pctfree 10
-  initrans 2
-  maxtrans 255
-  storage
+-- ADD COMMENTS TO THE COLUMNS
+COMMENT ON COLUMN QRTZ_TIMED_TASK.TASK_ID
+IS '任务编码';
+COMMENT ON COLUMN QRTZ_TIMED_TASK.TASK_NAME
+IS '任务名称';
+COMMENT ON COLUMN QRTZ_TIMED_TASK.TASK_DESC
+IS '任务描述';
+COMMENT ON COLUMN QRTZ_TIMED_TASK.TASK_EXPRES
+IS '任务执行表达式';
+COMMENT ON COLUMN QRTZ_TIMED_TASK.TASK_METHOD
+IS '任务执行方法';
+COMMENT ON COLUMN QRTZ_TIMED_TASK.TASK_CLASS
+IS '任务接口路径';
+COMMENT ON COLUMN QRTZ_TIMED_TASK.TASK_GROUP
+IS '任务分组';
+COMMENT ON COLUMN QRTZ_TIMED_TASK.STATUS
+IS '任务状态';
+COMMENT ON COLUMN QRTZ_TIMED_TASK.CREATE_TIME
+IS '创建时间';
+COMMENT ON COLUMN QRTZ_TIMED_TASK.CREATER
+IS '创建人员';
+COMMENT ON COLUMN QRTZ_TIMED_TASK.EXT1
+IS '扩展字段1';
+COMMENT ON COLUMN QRTZ_TIMED_TASK.EXT2
+IS '扩展字段2';
+COMMENT ON COLUMN QRTZ_TIMED_TASK.EXT3
+IS '扩展字段3';
+COMMENT ON COLUMN QRTZ_TIMED_TASK.EXT4
+IS '扩展字段4';
+-- CREATE/RECREATE PRIMARY, UNIQUE AND FOREIGN KEY CONSTRAINTS
+ALTER TABLE QRTZ_TIMED_TASK
+  ADD PRIMARY KEY (TASK_ID)
+  USING INDEX
+  PCTFREE 10
+  INITRANS 2
+  MAXTRANS 255
+  STORAGE
   (
-  initial 64K
-  next 1M
-  minextents 1
-  maxextents unlimited
+  INITIAL 64K
+  NEXT 1M
+  MINEXTENTS 1
+  MAXEXTENTS UNLIMITED
   );
-alter table QRTZ_TIMED_TASK
-  add unique (TASK_NAME)
-  using index
-  pctfree 10
-  initrans 2
-  maxtrans 255
-  storage
+ALTER TABLE QRTZ_TIMED_TASK
+  ADD UNIQUE (TASK_NAME)
+  USING INDEX
+  PCTFREE 10
+  INITRANS 2
+  MAXTRANS 255
+  STORAGE
   (
-  initial 64K
-  next 1M
-  minextents 1
-  maxextents unlimited
+  INITIAL 64K
+  NEXT 1M
+  MINEXTENTS 1
+  MAXEXTENTS UNLIMITED
   );
 
--- Create table
-create table QRTZ_TIMED_TASK_PARAM
+-- CREATE TABLE
+CREATE TABLE QRTZ_TIMED_TASK_PARAM
 (
-  param_id    INTEGER not null,
-  param_key   VARCHAR2(100) not null,
-  param_value VARCHAR2(1000) not null,
-  param_type  VARCHAR2(100),
-  param_desc  VARCHAR2(1000),
-  task_name   VARCHAR2(200) not NULL,
-  sort_id        INTEGER
+  PARAM_ID    INTEGER NOT NULL,
+  PARAM_KEY   VARCHAR2(100) NOT NULL,
+  PARAM_VALUE VARCHAR2(1000) NOT NULL,
+  PARAM_TYPE  VARCHAR2(100),
+  PARAM_DESC  VARCHAR2(1000),
+  TASK_NAME   VARCHAR2(200) NOT NULL,
+  SORT_ID        INTEGER
 );
--- Add comments to the columns
-comment on column QRTZ_TIMED_TASK_PARAM.param_id
-is '参数主键';
-comment on column QRTZ_TIMED_TASK_PARAM.param_key
-is '参数代码';
-comment on column QRTZ_TIMED_TASK_PARAM.param_value
-is '参数值';
-comment on column QRTZ_TIMED_TASK_PARAM.param_type
-is '参数类型(不填默认为String类型)';
-comment on column QRTZ_TIMED_TASK_PARAM.param_desc
-is '参数描述';
-comment on column QRTZ_TIMED_TASK_PARAM.task_name
-is '任务名称';
-comment on column QRTZ_TIMED_TASK_PARAM.sort_id
-is '参数顺序';
+-- ADD COMMENTS TO THE COLUMNS
+COMMENT ON COLUMN QRTZ_TIMED_TASK_PARAM.PARAM_ID
+IS '参数主键';
+COMMENT ON COLUMN QRTZ_TIMED_TASK_PARAM.PARAM_KEY
+IS '参数代码';
+COMMENT ON COLUMN QRTZ_TIMED_TASK_PARAM.PARAM_VALUE
+IS '参数值';
+COMMENT ON COLUMN QRTZ_TIMED_TASK_PARAM.PARAM_TYPE
+IS '参数类型(不填默认为STRING类型)';
+COMMENT ON COLUMN QRTZ_TIMED_TASK_PARAM.PARAM_DESC
+IS '参数描述';
+COMMENT ON COLUMN QRTZ_TIMED_TASK_PARAM.TASK_NAME
+IS '任务名称';
+COMMENT ON COLUMN QRTZ_TIMED_TASK_PARAM.SORT_ID
+IS '参数顺序';
 
--- Create/Recreate primary, unique and foreign key constraints
-alter table QRTZ_TIMED_TASK_PARAM
-  add primary key (PARAM_ID)
-  using index
-  pctfree 10
-  initrans 2
-  maxtrans 255
-  storage
+-- CREATE/RECREATE PRIMARY, UNIQUE AND FOREIGN KEY CONSTRAINTS
+ALTER TABLE QRTZ_TIMED_TASK_PARAM
+  ADD PRIMARY KEY (PARAM_ID)
+  USING INDEX
+  PCTFREE 10
+  INITRANS 2
+  MAXTRANS 255
+  STORAGE
   (
-  initial 64K
-  next 1M
-  minextents 1
-  maxextents unlimited
+  INITIAL 64K
+  NEXT 1M
+  MINEXTENTS 1
+  MAXEXTENTS UNLIMITED
   );
+ALTER TABLE QRTZ_TIMED_TASK
+  ADD UNIQUE (PARAM_KEY,TASK_NAME)
+  USING INDEX
+  PCTFREE 10
+  INITRANS 2
+  MAXTRANS 255
+  STORAGE
+  (
+  INITIAL 64K
+  NEXT 1M
+  MINEXTENTS 1
+  MAXEXTENTS UNLIMITED
+  );
+
+CREATE SEQUENCE SEQ_TASK
+MINVALUE 1
+START WITH 1
+NOMAXVALUE
+INCREMENT BY 1
+NOCYCLE
+CACHE 30;
+
+CREATE SEQUENCE SEQ_TASK_PARAM
+MINVALUE 1
+START WITH 1
+NOMAXVALUE
+INCREMENT BY 1
+NOCYCLE
+CACHE 30;
