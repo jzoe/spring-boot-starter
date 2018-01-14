f7925e301f15330dd24969e68b2c9c38e7576b1f
15221499301@163.com <15221499301@163.com>
2018-01-15 00:47:35 +0800

    .

--- spring-boot-starter-quartz-examples/src/test/java/com/github/quartz/examples/QuartzApplicationTests.java
+++ spring-boot-starter-quartz-examples/src/test/java/com/github/quartz/examples/QuartzApplicationTests.java
@@ -3,16 +3,15 @@ package com.github.quartz.examples;
 import com.github.quartz.jdbc.QuartzRepository;
 import com.github.quartz.model.entity.QrtzTimedTask;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcOperations;
-import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class QuartzApplicationTests {
 
     @Autowired
@@ -28,5 +27,14 @@ public class QuartzApplicationTests {
 
     @Test
     public void test02() throws Exception {
+        List<String> ipAddress = new ArrayList<String>();
+        ipAddress.add("192.168.5.248");
+        ipAddress.add("192.168.6.248");
+        ipAddress.add("192.168.7.248");
+        for (int i = 0; i < 10; i++) {
+            Random random = new Random();
+            int number = random.nextInt(ipAddress.size());
+            System.out.println(ipAddress.get(number));
+        }
     }
 }
