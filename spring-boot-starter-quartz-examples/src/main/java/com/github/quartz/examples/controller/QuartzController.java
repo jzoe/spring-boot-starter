a36d4135d0f256318cb51d4ac3cfa52083af2983
15221499301@163.com <15221499301@163.com>
2018-01-15 23:12:21 +0800

    。

--- spring-boot-starter-quartz-examples/src/main/java/com/github/quartz/examples/controller/QuartzController.java
+++ spring-boot-starter-quartz-examples/src/main/java/com/github/quartz/examples/controller/QuartzController.java
@@ -2,7 +2,7 @@ package com.github.quartz.examples.controller;
 
 import com.github.quartz.examples.util.JsonUtil;
 import com.github.quartz.model.entity.QrtzTimedTask;
-import com.github.quartz.utils.ScheduleUtil;
+import com.github.quartz.schedule.util.ScheduleUtil;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
