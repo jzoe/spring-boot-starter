fe79a21ad1c6453c400a96889ac74fc18f4ad2e8
15221499301@163.com <15221499301@163.com>
2018-01-18 14:47:49 +0800

    .

--- spring-boot-starter-quartz-examples/src/test/java/com/github/quartz/examples/QuartzApplicationTests.java
+++ spring-boot-starter-quartz-examples/src/test/java/com/github/quartz/examples/QuartzApplicationTests.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.ArrayList;
@@ -15,6 +16,7 @@ import java.util.Random;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("dev")
 public class QuartzApplicationTests {
 
     @Autowired
